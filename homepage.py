9cf4b2a95b2dc72e8685e765b704effb8af91334
thaibinh0203 <binhthai0203@gmail.com>
2025-11-18 17:40:21 +0700

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -31,6 +31,9 @@ div.stButton > button:hover {
     transform: translateY(-2px);
 }
 </style>
+""", unsafe_allow_html=True)
+
+</style>
 # 3. CSS
 st.markdown("""
 <style>
@@ -305,6 +308,7 @@ with col1:
 with col2:
     if st.button("Analyze Reviews", use_container_width=True):
         st.switch_page("pages/review.py") 
+
 
 
 
